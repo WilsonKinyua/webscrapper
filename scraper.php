efca2efebe749ce8fce89e8e8a7eb640fb520a04
wilson <wilsonkinyuam@gmail.com>
2020-09-10 21:58:20 +0300

    final

--- scraper.php
+++ scraper.php
@@ -4,11 +4,9 @@ $city=$_GET['city'];
 
 $city=str_replace(" ", "", $city);
 
-$contents=file_get_contents("http://www.weather-forecast.com/locations/".$city."/forecasts/latest");
+$contents=file_get_contents("https://www.weather-forecast.com/locations/" . $city . "/forecasts/latest");
 
 preg_match('/3 Day Weather Forecast Summary:<\/b> 
         <span class="phrase">(.*?)</s', $contents, $matches);
 
 echo $matches[1];
-
-?>
